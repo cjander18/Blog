c559f491df33fcba2f0a592e57e0df328e9050f1
Clinton Anderson <Clinton Anderson>
2016-05-06 06:14:15 -0400

    Basic Delegate example

---
+++ Code Examples/Delegates/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DelegateExample
+{
+
+    public delegate void ADelegate();
+
+    public delegate int AnIntReturnDelegate();
+
+    public delegate void AnIntParamDelegate(int intNumber);
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            // Build a vanilla delegate
+            ADelegate TestADelegate = new ADelegate(DoTheThing);
+
+            // Call a vanilla delegate
+            TestADelegate();
+            
+            // Build a delegate that returns a value
+            AnIntReturnDelegate TestAnIntReturnDelegate = new AnIntReturnDelegate(DoTheIntThing);
+
+            // Get a return value from the delegate
+            int intReturnValue = TestAnIntReturnDelegate();
+            Console.WriteLine(string.Format("It returned {0}!", intReturnValue));
+            
+            // Build a delegate that takes a parameter
+            AnIntParamDelegate TestAnIntParamDelegate = new AnIntParamDelegate(DoTheIntParamThing);
+
+            // Pass a parameter to the delegate
+            TestAnIntParamDelegate(42);
+
+            Console.ReadLine();
+
+        }
+
+        public static void DoTheThing()
+        {
+
+            Console.WriteLine("It did the thing!");
+
+        }
+
+        public static int DoTheIntThing()
+        {
+
+            Console.WriteLine("It did the int thing!");
+
+            return 0;
+
+        }
+
+        public static void DoTheIntParamThing(int intNum)
+        {
+
+            Console.WriteLine(string.Format("It did the thing {0} times!", intNum));
+
+        }
+    }
+}
---
+++ Code Examples/Delegates/DelegateExample/OverloadedDelegateExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OverloadedDelegateExample
+{
+    public delegate void AnOverloadedDelegate(int intNum, string strVerb);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AnOverloadedDelegate delOverload = null;
+
+            // Note, here I'm implementing a simplified event/listener design pattern
+            delOverload += NotifyListenerVersionOne;
+            delOverload += NotifyListenerVersionTwo;
+
+            // Note, that the methods will be called in the order they were added.
+            // So this will call NotifyListenerVersionOne then NotifyListenerVersionTwo.
+            // If we had added them in the reverse order, their call order would also
+            // be reversed.
+            delOverload(3, "jumped");
+
+            Console.ReadLine();
+
+        }
+
+        public static void NotifyListenerVersionOne(int intNum, string strVerb)
+        {
+
+            string strNotify = string.Format("It {0} {1} times!", strVerb, intNum);
+            Console.WriteLine(strNotify);
+            // You could then notify anyone/thing listening to version one of your project/api here!
+
+        }
+
+        public static void NotifyListenerVersionTwo(int intNum, string strVerb)
+        {
+
+            int intNewNum = intNum * intNum;
+            string strNotify = string.Format("It {0} {1} times!", strVerb, intNewNum);
+            Console.WriteLine(strNotify);
+            // You could then notify anyone/thing listening to version two of your project/api here!
+
+        }
+    }
+}
